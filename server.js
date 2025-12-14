17303a9ab76fa3b48e5b73fe2790ce410b6402f3
Omkar Nath <nathomkar799@gmail.com>
2025-12-14 15:26:06 +0530

    Add basic express. Add mongoose in the dependencies.

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req,res)=>{
+    res.json({msg:"API hit"});
+})
+
+const PORT = 8000
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running at ${PORT}. ✅` );
+})
